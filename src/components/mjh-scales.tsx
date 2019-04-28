5ae4c9cbbaac236a8dad4bb6d229508be6716b30
Matt Hamrick <matthewjhamrick@gmail.com>
2019-04-28 03:27:34 +0000

    got this working a bit better. Should move to tonal next.

--- src/components/mjh-scales.tsx
+++ src/components/mjh-scales.tsx
@@ -1,5 +1,17 @@
 import * as React from "react";
+import { Pitch } from "../types/pitch";
 import styled from "styled-components";
+
+const colors = {
+  base03: "#002b36",
+  base02: "#073642",
+  base01: "#586e75",
+  base00: "#657b83",
+  base0: "#839496",
+  base1: "#93a1a1",
+  base2: "#eee8d5",
+  base3: "#fdf6e3"
+};
 
 const ScrollRelative = styled.div`
   overflow: scroll;
@@ -12,6 +24,7 @@ const NoWidth = styled.div`
 
 const PianoWrapper = styled.div`
   display: inline-flex;
+  margin: 2px;
 `;
 
 // black key standard size
@@ -27,17 +40,30 @@ const whiteKeyWidth = 24;
 const blackKeyHeight = 90;
 const blackKeyWidth = 12;
 
-const marginLeft = ({ keyType = "white" }) =>
-  keyType === "white" ? "unset" : `-${blackKeyWidth / 2}px`;
+const marginLeft = ({ keyType = "white", scale = 1 }) =>
+  keyType === "white" ? "unset" : `-${(blackKeyWidth / 2) * scale}px`;
 
-const width = ({ keyType = "white" }) =>
-  keyType === "white" ? whiteKeyWidth : blackKeyWidth;
+const width = ({ keyType = "white", scale = 1 }) =>
+  (keyType === "white" ? whiteKeyWidth : blackKeyWidth) * scale;
 
-const height = ({ keyType = "white" }) =>
-  keyType === "white" ? whiteKeyHeight : blackKeyHeight;
+const height = ({ keyType = "white", scale = 1, yScale = 1 }) =>
+  (keyType === "white" ? whiteKeyHeight : blackKeyHeight) * scale * yScale;
 
-const backgroundColor = ({ keyType = "white" }) =>
-  keyType === "white" ? "#fdf6e3" : "#002b36";
+const backgroundColor = ({ keyType = "white", highlight = false }) => {
+  if (keyType === "white") {
+    if (highlight) {
+      return colors.base3;
+    } else {
+      return colors.base2;
+    }
+  } else {
+    if (highlight) {
+      return colors.base03;
+    } else {
+      return colors.base02;
+    }
+  }
+};
 
 const position = ({ keyType = "white" }) =>
   keyType === "white" ? "unset" : "absolute";
@@ -75,60 +101,43 @@ class ScrollMe extends React.Component {
   }
 }
 
-const Fingering = styled.div`
+const KeyInfo = styled.div`
   text-align: center;
-  margin-bottom: 20px;
   color: #657b83;
 `;
 
-const BlackKey = (props) => {
-  return (
-    <NoWidth>
-      <Key {...props} keyType="black" />
-    </NoWidth>
-  );
-};
-
-// TODO add scrollIntoView for element 44
-// temp1.scrollIntoView({behavior: 'smooth', inline: 'center'})
-
-enum Pitch {
-  A = 0,
-  A_S = 1,
-  B = 2,
-  C = 3,
-  C_S = 4,
-  D = 5,
-  D_s = 6,
-  E = 7,
-  F = 8,
-  F_S = 9,
-  G = 10,
-  G_S = 11
-}
+const Fingering = styled.div`
+  color: ${({ hand }) => (hand === "left" ? "#268bd2" : "#859900")};
+`;
 
 enum KeyType {
   WHITE = "white",
   BLACK = "black"
 }
 
-const pianoRange: number[] = Array.apply(null, { length: 88 }).map(
+const pianoRange: Pitch[] = Array.apply(null, { length: 88 }).map(
   (_, idx: number) => idx
 );
 
-const Piano = ({ keyChildren = {} }) => (
+const Piano = ({
+  keyChildren = {},
+  scale,
+  yScale,
+  keyFilter = (pitch: Pitch) => true,
+  highlight = (pitch: Pitch) => false
+}) => (
   <PianoWrapper>
-    {pianoRange.map((_, idx) => {
+    {pianoRange.filter(keyFilter).map((pitch, _) => {
       let keyType: KeyType;
-      const pitch: Pitch = idx % 12;
-      switch (pitch) {
-        case Pitch.A:
-        case Pitch.B:
-        case Pitch.C:
-        case Pitch.D:
-        case Pitch.E:
-        case Pitch.F:
-        case Pitch.G:
+      const normalizedPitch: Pitch = pitch % 12;
+      switch (normalizedPitch) {
+        case Pitch.A0:
+        case Pitch.B0:
+        case Pitch.C0:
+        case Pitch.D0:
+        case Pitch.E0:
+        case Pitch.F0:
+        case Pitch.G0:
           keyType = KeyType.WHITE;
           break;
         default:
@@ -136,15 +145,22 @@ const Piano = ({ keyChildren = {} }) => (
           break;
       }
       let key = (
-        <Key key={idx} className={idx} keyType={keyType}>
-          {keyChildren[idx] && keyChildren[idx]}
+        <Key
+          key={pitch}
+          className={pitch}
+          keyType={keyType}
+          scale={scale}
+          yScale={yScale}
+          highlight={highlight(pitch)}
+        >
+          {keyChildren[pitch] && keyChildren[pitch]}
         </Key>
       );
       if (keyType === "black") {
-        key = <NoWidth key={idx}>{key}</NoWidth>;
+        key = <NoWidth key={pitch}>{key}</NoWidth>;
       }
-      if (idx === 44) {
-        key = <ScrollMe key={idx}>{key}</ScrollMe>;
+      if (pitch === Pitch.E4) {
+        key = <ScrollMe key={pitch}>{key}</ScrollMe>;
       }
       return key;
     })}
@@ -153,30 +169,86 @@ const Piano = ({ keyChildren = {} }) => (
 
 const keyFilter = (pitches: Set<Pitch>) => (pitch) => pitches.has(pitch % 12);
 
-const A_Major = keyFilter(
-  new Set([Pitch.A, Pitch.B, Pitch.C_S, Pitch.D, Pitch.E, Pitch.F_S, Pitch.G_S])
-);
+const A_Major_Pitches = [
+  Pitch.A0,
+  Pitch.B0,
+  Pitch["C#0"],
+  Pitch.D0,
+  Pitch.E0,
+  Pitch["F#0"],
+  Pitch["G#0"]
+];
 
-const A_Major_Fingering = {
-  [Pitch.A]: 1,
-  [Pitch.B]: 2,
-  [Pitch.C_S]: 3,
-  [Pitch.D]: 1,
-  [Pitch.E]: 2,
-  [Pitch.F_S]: 3,
-  [Pitch.G_S]: 4
+const A_Major = keyFilter(new Set(A_Major_Pitches));
+
+const A_Major_Right_Hand = {
+  [Pitch.A0]: 1,
+  [Pitch.B0]: 2,
+  [Pitch["C#0"]]: 3,
+  [Pitch.D0]: 1,
+  [Pitch.E0]: 2,
+  [Pitch["F#0"]]: 3,
+  [Pitch["G#0"]]: 4,
+  [Pitch.A1]: 1,
+  [Pitch.B1]: 2,
+  [Pitch["C#1"]]: 3,
+  [Pitch.D1]: 1,
+  [Pitch.E1]: 2,
+  [Pitch["F#1"]]: 3,
+  [Pitch["G#1"]]: 4,
+  [Pitch["A#2"]]: 5
+};
+
+const A_Major_Left_Hand = {
+  [Pitch.A0]: 5,
+  [Pitch.B0]: 4,
+  [Pitch["C#0"]]: 3,
+  [Pitch.D0]: 2,
+  [Pitch.E0]: 1,
+  [Pitch["F#0"]]: 3,
+  [Pitch["G#0"]]: 2,
+  [Pitch.A1]: 1,
+  [Pitch.B1]: 4,
+  [Pitch["C#1"]]: 3,
+  [Pitch.D1]: 2,
+  [Pitch.E1]: 1,
+  [Pitch["F#1"]]: 3,
+  [Pitch["G#1"]]: 2,
+  [Pitch["A#2"]]: 1
 };
 
 export default () => (
   <ScrollRelative>
-    <Piano
-      keyChildren={pianoRange.filter(A_Major).reduce(
-        (acc, pitch) =>
-          Object.assign(acc, {
-            [pitch]: <Fingering>{A_Major_Fingering[pitch % 12]}</Fingering>
-          }),
-        {}
-      )}
-    />
+    {[[A_Major_Left_Hand, "L"], [A_Major_Right_Hand, "R"]].map(
+      ([fingering, label]) => (
+        <div style={{ display: "flex" }}>
+          <div
+            style={{
+              alignSelf: "center",
+              marginRight: "2px",
+              minWidth: "10px"
+            }}
+          >
+            {label}
+          </div>
+          <Piano
+            keyFilter={(key: Pitch) => key <= Pitch.A4}
+            yScale={0.5}
+            highlight={A_Major}
+            keyChildren={pianoRange.filter(A_Major).reduce(
+              (acc, pitch) =>
+                Object.assign(acc, {
+                  [pitch]: (
+                    <KeyInfo>
+                      <Fingering hand="left">{fingering[pitch]}</Fingering>
+                    </KeyInfo>
+                  )
+                }),
+              {}
+            )}
+          />
+        </div>
+      )
+    )}
   </ScrollRelative>
 );
